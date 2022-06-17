b08de02a2f7d172a8f817894b915bcef33e95fd6
sawadyecma <snowslidez@yahoo.co.jp>
2022-06-17 09:42:42 +0900

    add upload path

--- file-server/src/index.ts
+++ file-server/src/index.ts
@@ -1,14 +1,28 @@
 import fastify from "fastify";
 import fastifyStatic from "@fastify/static";
+import fastifyFileUpload from "fastify-file-upload";
 import path from "path";
 
 const server = fastify();
+
+const storageBasepath = "../public";
+
 server.register(fastifyStatic, {
-  root: path.join(__dirname, "../public"),
+  root: path.join(__dirname, storageBasepath),
+});
+server.register(fastifyFileUpload);
+
+server.get("/ping", async () => {
+  return "pongpongpaaaaaong\n";
 });
 
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
+server.post("/upload", function (req, res) {
+  // @ts-ignore
+  const file = req.raw.files["image"];
+
+  file.mv(path.join(__dirname, storageBasepath, file.name));
+
+  res.send(file);
 });
 
 server.listen({ port: 8080 }, (err, address) => {
